527659aafdf3e8f58bae0e726efc48c5cf4a7d14
0xyusufz <gurucoc471@gmail.com>
2026-02-12 17:16:51 +0530

    all mathemartical and membership operation in set

---
+++ python3.py
@@ -0,0 +1,8 @@
+a = {1,2,3,4,5}
+b = {4,5,6,7,8}
+print(a|b) #all elem but without duplicate
+print(a&b) #common
+print(10 in a) # includes or not
+print(a-b) # that elem which is in a but not in b
+print(b-a) # that elem which is in b but not in a
+print(a^b) # only unique no duplicate 
