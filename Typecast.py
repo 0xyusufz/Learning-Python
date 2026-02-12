1e319c81049630b7aeac4d2733a7ed49b9736936
0xyusufz <gurucoc471@gmail.com>
2026-02-12 16:50:22 +0530

    type casting

---
+++ Typecast.py
@@ -0,0 +1,47 @@
+# int()
+
+
+a = "10"
+b = 10.2
+ty = int(a)   #10
+ty2 = int(b)  #10
+print(type(ty)) #int
+print(type(ty2)) #int
+
+# float
+
+c = 10
+d= "10.2"
+ty3 = float(c) 
+print(ty3)
+print(type(ty3))
+ty4 = float(d)
+print(ty4)
+print(type(ty4))
+
+# complex
+
+e = 1
+ty5 = complex(e)
+print(type(ty5))
+f = 1+5j
+print(type(f))
+
+
+#string
+
+g = 1
+ty6 = str(g)
+print(type(ty6))
+
+
+
+#boolean
+
+
+h =0
+i =1
+ty7 = bool(h)
+print(ty7)
+ty8 = bool(i)
+print(ty8)
